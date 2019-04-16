698f3c2a586a8681ed56a18dea96c7fae7730e64
Sangkon Han <sigmadream@gmail.com>
2019-04-16 11:53:25 +0900

    Added 초기 개발 설정

---
+++ zambit.ts
@@ -0,0 +1,11 @@
+//% weight=100 color=#000000 icon="\uf06e" block="Zam:bit"
+namespace zambit {
+    /**
+     * Setup zam:bit,
+     * must be called before using the display
+     */
+    export function setup(): void {
+    }
+}
+
+zambit.setup();
